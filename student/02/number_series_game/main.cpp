929a664dad578c911d329c86c3e4b636742f8f57
Vili Ikäheimo <vili.ikaheimo@tuni.fi>
2022-01-17 16:17:01 +0200

    lukusarjapelin toinen versio

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -4,6 +4,18 @@
 int main()
 {
     std::cout << "How many numbers would you like to have? ";
-
+    int lukumaara = 0;
+    std::cin >> lukumaara;
+    for (int luku = 1; luku <= lukumaara; ++luku){
+        if (luku % 21 == 0) {
+            std::cout << "Zip Boing" << std::endl;
+        } else if (luku % 3 == 0){
+            std::cout << "Zip" << std::endl;
+        } else if (luku % 7 == 0){
+            std::cout << "Boing" << std::endl;
+        } else {
+            std::cout << luku << std::endl;
+        }
+    }
     return 0;
 }
