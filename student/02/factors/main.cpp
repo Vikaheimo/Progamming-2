9b6c188f77ce59548e5d5a8c95aaed2fb348ca89
Vili Ikäheimo <vili.ikaheimo@tuni.fi>
2022-01-17 17:21:45 +0200

    ensimmäinen versio lähimmät tekijät ohjelmasta

---
+++ student/02/factors/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter a positive number: ";
+    int numero = 0;
+    cin >> numero;
+    if (numero < 1){
+        cout << "Only positive numbers accepted" << endl;
+    } else {
+        int num1 = numero;
+        int num2 = 1;
+        for (int luku1 = 1; luku1 <= numero; ++luku1){
+            for (int luku2 = 1; luku2 <= numero; ++luku2){
+                if (luku1 * luku2 == numero){
+                    if (abs(num1 - num2) > abs(luku1 - luku2)){
+                        num1 = luku1;
+                        num2 = luku2;
+                    }
+
+                }
+            }
+        }
+        cout << numero << " = " << num1 << " * " << num2 << endl;
+    }
+    return 0;
+}
