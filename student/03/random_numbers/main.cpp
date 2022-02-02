eae3492f99e34cf898c8b88aaff7a5748c203408
Vili Ikäheimo <vili.ikaheimo@tuni.fi>
2022-02-02 15:01:09 +0200

    Satunnaislukuja-ohjlema

---
+++ student/03/random_numbers/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <random>
+#include <string>
+
+using namespace std;
+
+void produce_random_numbers(unsigned int lower, unsigned int upper){
+    default_random_engine gen(42);
+    uniform_int_distribution<int> distr(lower, upper);
+    string valinta;
+    while (valinta !="q"){
+        cout << endl << "Your drawn random number is " << distr(gen) << endl << "Press q to quit or any other key to continue: ";
+        cin >> valinta;
+
+    }
+}
+
+int main()
+{
+    unsigned int lower_bound, upper_bound;
+    cout << "Enter a lower bound: ";
+    cin >> lower_bound;
+    cout << "Enter an upper bound: ";
+    cin >> upper_bound;
+
+    if(lower_bound >= upper_bound)
+    {
+        cout << "The upper bound must be strictly greater than the lower bound"
+             << endl;
+        return EXIT_FAILURE;
+    }
+
+    produce_random_numbers(lower_bound, upper_bound);
+
+
+    return EXIT_SUCCESS;
+}
