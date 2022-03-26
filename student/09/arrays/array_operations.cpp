0fb5125fa174c03f32651160d953744cecbff2ff
Vili Ikäheimo <vili.ikaheimo@tuni.fi>
2022-03-26 13:49:29 +0200

    Added method greatest_v2
    - Greatest_v2 seaches for the biggest elment in a array from start poiner to the end pointer

--- student/09/arrays/array_operations.cpp
+++ student/09/arrays/array_operations.cpp
@@ -1,1 +1,31 @@
 #include "array_operations.hh"
+
+int greatest_v1(int *itemptr, int size)
+{
+    return 0;
+}
+
+int greatest_v2(int *itemptr, int *endptr)
+{
+    int isoin_luku = 0;
+    int* taulukko_osoitin = itemptr;
+
+    while(taulukko_osoitin != endptr){
+        if (*taulukko_osoitin > isoin_luku){
+
+            isoin_luku = *taulukko_osoitin;
+        }
+        taulukko_osoitin++;
+    }
+    return isoin_luku;
+}
+
+void copy(int *itemptr, int *endptr, int *targetptr)
+{
+
+}
+
+void reverse(int *leftptr, int *rightptr)
+{
+
+}
