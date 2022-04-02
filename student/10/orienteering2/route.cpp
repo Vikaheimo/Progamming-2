e579a18319c7555c195e16b5a86155ae4b025cce
Vili Ikäheimo <vili.ikaheimo@tuni.fi>
2022-04-02 15:00:18 +0300

    Added implementation to class route

--- student/10/orienteering2/route.cpp
+++ student/10/orienteering2/route.cpp
@@ -1,6 +1,139 @@
+/*
+#############################################################################
+# COMP.CS.110 Ohjelmointi 2: Rakenteet / Programming 2: Structures          #
+# Project3: Suunnistus / Orienteering                                       #
+# File: route.cpp                                                           #
+# Description: Implementation for the class route                           #
+# Notes:                                                                    #
+# Program author:                                                           #
+#       Name: Vili Ikäheimo                                                 #
+#       Student number: 150286761                                           #
+#       UserID: csviik                                                      #
+#       E-Mail: vili.ikaheimo@tuni.fi                                       #
+#############################################################################
+*/
 #include "route.hh"
 
-Route::Route()
-{
 
+Route::Route(std::string name):
+name_(name) {
+
+    // set lenght of the route to zero, so that the value can
+    // still be used
+    lenghtOfRoute_ = 0;
+}
+
+bool Route::addPoint(Point* new_point)
+{
+    // check if the point allready exists in the route
+    // and return false if it does
+    if (IsPoint(new_point)){
+        return false;
+    }
+
+    // add point to the route and return true
+    route_.push_back(new_point);
+    return true;
+
+}
+
+std::vector<std::string> Route::getPoints() const
+{
+    std::vector<std::string> point_names;
+
+    // loop through the route points
+    for(auto& point : route_){
+
+        // add each point name to the list
+        std::string point_name = point->getName();
+        point_names.push_back(point_name);
+    }
+
+    return point_names;
+}
+
+int Route::getLength()
+{
+    // check if the lenght of the route hasn't been calculated yet
+    if (lenghtOfRoute_ == 0){
+        CalculateLenght();
+    }
+
+    return lenghtOfRoute_;
+}
+
+int Route::GreatestRise(Point *control_point) const
+{
+    // return -1 if the point doesn't exist
+    if (!IsPoint(control_point)){
+        return -1;
+    }
+
+    int height_at_start = control_point->getHeight();
+    int last_height = 0;
+    bool is_found = false;
+
+    for (auto& point : route_){
+        int current_height = point->getHeight();
+
+        // set flag true true if the point is found
+        if (point == control_point){
+           is_found = true;
+
+        // if the point is not found skip over
+        } else if (!is_found) {
+            continue;
+
+        // break out of the code if the height gets smaller
+        } else if (current_height > last_height) {
+            break;
+
+        }
+
+        // store last height value
+        last_height = current_height;
+    }
+    return last_height - height_at_start;
+}
+
+void Route::CalculateLenght()
+{
+    std::vector<int> last_coords = route_.front()->getCoordinates();
+    double length = 0;
+    for(auto& point : route_){
+
+        // get current coordinates and their x and y coordinates
+        std::vector<int> current_coords = point->getCoordinates();
+        int current_x = current_coords.front();
+        int current_y = current_coords.back();
+
+        // get the coordinates of the last point
+        int last_x = last_coords.front();
+        int last_y = last_coords.back();
+
+        // get the distance between x coordinates and y coordinates
+        int distance_x = pow((current_x - last_x), 2);
+        int distance_y = pow((current_y - last_y), 2);
+
+        // calculate distance and add it to the lenght
+        length += std::sqrt((distance_x + distance_y));
+
+        last_coords = current_coords;
+    }
+    // update route lenght
+    lenghtOfRoute_ = length;
+}
+
+bool Route::IsPoint(Point *control_point) const
+{
+    // loop through the route_ vector
+    for(auto& point : route_){
+
+        // check if the value is the same as the one we are
+        // looking for
+        if(control_point == point){
+            return true;
+        }
+    }
+    return false;
 }
--- student/10/orienteering2/route.hh
+++ student/10/orienteering2/route.hh
@@ -3,25 +3,60 @@
 # COMP.CS.110 Ohjelmointi 2: Rakenteet / Programming 2: Structures          #
 # Project3: Suunnistus / Orienteering                                       #
 # File: route.hh                                                            #
-# Description: Datastructure that represents an route and handles           #
+# Description: Datastructure that represents a route and handles            #
 # route information                                                         #
 # Notes:                                                                    #
 # Program author:                                                           #
 #       Name: Vili Ikäheimo                                                 #
 #       Student number: 150286761                                           #
 #       UserID: csviik                                                      #
-#       E-Mail: vili.ikaheimo@tuni.fi
+#       E-Mail: vili.ikaheimo@tuni.fi                                       #
 #############################################################################
 */
 #ifndef ROUTE_HH
 #define ROUTE_HH
 
+#include "vector"
+#include "string"
+#include "point.hh"
+#include "cmath"
 
 class Route
 {
 public:
-    Route();
-    ~Route();
+    // constructor
+    Route(std::string name);
+
+    // method that adds a control point to the end of the route
+    // returns true if it was succesfull otherwise false
+    bool addPoint(Point* new_point);
+
+    // a method that returns the names of the control points in a route
+    std::vector<std::string> getPoints() const;
+
+    // a method that returns the lenght of the route
+    int getLength();
+
+    // a method that returns the highest rise from a given point
+    // if the point doesnt exist, returns -1 and if there is no rise
+    // after a given point return 0.
+    int GreatestRise(Point* control_point) const;
+
+
+
+private:
+    // add a vector that has all the command points that are in the route
+    std::vector<Point*> route_;
+
+    std::string name_;
+    double lenghtOfRoute_;
+
+    // calculates the length of the route
+    void CalculateLenght();
+
+    // method that returns true if a point is in the route
+    // otherwise returns false
+    bool IsPoint(Point* control_point) const;
 };
 
 #endif // ROUTE_HH
