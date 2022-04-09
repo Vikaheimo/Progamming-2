0b915824b18f59a733d7f6652018df8717fd3ac4
Vili Ikäheimo <vili.ikaheimo@tuni.fi>
2022-04-09 14:08:00 +0300

    Find dialog v1

--- student/11/find_dialog/mainwindow.cpp
+++ student/11/find_dialog/mainwindow.cpp
@@ -6,10 +6,69 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->closePushButton, &QPushButton::clicked, this, &MainWindow::close);
+
+    connect(ui->findPushButton, &QPushButton::clicked, this, &MainWindow::onFindCommand);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::onFindCommand()
+{
+    std::string file_name = ui->fileLineEdit->text().toUtf8().constData();
+    std::string etsi_string = ui->keyLineEdit->text().toUtf8().constData();
+
+    std::string text;
+    if (!get_file_data(file_name)){
+        text = "File not found";
+
+    } else if (etsi_string.length() == 0) {
+        text = "File found";
+
+    }else if (!check_data(etsi_string, ui->matchCheckBox->isChecked())) {
+        text = "Word not found";
+
+    } else {
+        text = "Word found";
+    }
+    ui->textBrowser->setText(QString::fromStdString(text));
+}
+
+bool MainWindow::get_file_data(std::string file_name)
+{
+    std::ifstream file;
+    file.open(file_name);
+
+    if (!file){
+        return false;
+        file.close();
+    }
+    std::string line;
+    while(getline(file, line)){
+        file_data_.append(" "+line);
+    }
+    file.close();
+    return true;
+}
+
+
+bool MainWindow::check_data(std::string word_to_search, bool match_case)
+{
+    if (!match_case){
+        std::for_each(word_to_search.begin(), word_to_search.end(), [](char & c){
+            c = std::toupper(c);
+        });
+        std::for_each(file_data_.begin(), file_data_.end(), [](char & c){
+            c = std::toupper(c);
+        });
+    }
+    if (file_data_.find(word_to_search) != std::string::npos){
+        return true;
+    }
+    return false;
 }
 
--- student/11/find_dialog/mainwindow.hh
+++ student/11/find_dialog/mainwindow.hh
@@ -2,7 +2,9 @@
 #define MAINWINDOW_HH
 
 #include <QMainWindow>
-
+#include <string>
+#include <fstream>
+#include <set>
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -15,7 +17,19 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void onFindCommand();
+
 private:
     Ui::MainWindow *ui;
+
+    std::string file_data_;
+    bool match_case_;
+
+    bool get_file_data(std::string file_name);
+    bool check_data(std::string word_to_search, bool match_case);
+
+    std::set<std::string> split(std::string text, char separator, bool skip=false);
+
 };
 #endif // MAINWINDOW_HH
