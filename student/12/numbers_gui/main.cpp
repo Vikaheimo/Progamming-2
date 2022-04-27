9a59bf30cff72cb4b9e7a6abf4e4fab604d014c2
Vili Ikäheimo <vili.ikaheimo@tuni.fi>
2022-04-27 15:58:14 +0300

    Added classes GameBoard and NumberTile

--- student/12/numbers_gui/main.cpp
+++ student/12/numbers_gui/main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * COMP.CS.110 Ohjelmointi 2: Rakenteet / Programming 2: Structures
  * Project4:  Peli nimeltä 2048 / Game called 2048
@@ -8,7 +7,7 @@
  * The progam is a game called 2048, in which the idea is to stack
  * tiles to get a high socre. The stacking can be done with the use of
  * arrow keys or wasd. Same numbers can only be stacked. The game ends
- *  when you cannot stack more titles, eg the pieces get "stuck".
+ * when you cannot stack more titles, eg the pieces get "stuck".
  *
  *
  * Progmam author:
@@ -20,8 +19,6 @@
  * Notes about the progam and it's implementation:
 */
 
-=======
->>>>>>> e6336a74326f1c906720abf7916c97777fadd926
 #include "mainwindow.hh"
 
 #include <QApplication>
